c6066592cebd92a94543f199c94a253a398f2b5c
lenagend <57205046+lenagend@users.noreply.github.com>
2023-12-04 19:45:13 +0900

    도커컴포즈 구성완료

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
 import time
-
 import schedule
-from dotenv import load_dotenv
 
 from src.database.database_manager import DatabaseManager
 from src.collectors.lotto_data_collector import LottoDataCollector
@@ -13,19 +10,6 @@ from src.updater.weekly_lotto_updater import LottoUpdater
 
 
 def main():
-    # 로컬용
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--env', help='환경 설정 (development 또는 production)', default='development')
-    # args = parser.parse_args()
-    #
-    # # 환경에 따라 .env 파일 선택
-    # if args.env == 'production':
-    #     dotenv_path = '.env.production'
-    # else:
-    #     dotenv_path = '.env.development'
-    #
-    # load_dotenv(dotenv_path=dotenv_path)
-    #
     with DatabaseManager() as db_manager:
         db_manager.setup_database()
 
@@ -36,13 +20,14 @@ def main():
         data_saver = LottoDataSaver(db_manager)
         updater = LottoUpdater(data_accessor, data_collector, number_generator, data_saver)
 
-        for draw_no in range(1, 20):
+        for draw_no in range(1, 1097):
             data = data_collector.get_lotto_data(draw_no)
             if data:
                 data_saver.save_lotto_data(data)
 
-        # schedule.every().sunday.at("00:00").do(updater.fetch_and_save_next_draw_data)
-        schedule.every(10).seconds.do(updater.fetch_and_save_next_draw_data)
+        updater.generate_and_save_numbers()
+        schedule.every().sunday.at("00:00").do(updater.fetch_and_save_next_draw_data)
+        # schedule.every(10).seconds.do(updater.fetch_and_save_next_draw_data)
 
         while True:
             schedule.run_pending()
